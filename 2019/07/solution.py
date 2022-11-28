b41b876d0422b7f73a7f41e67e7a61214713004b
Benjamin Charlton <kuldrgames@gmail.com>
2022-11-28 07:34:24 +0000

    Started 2019 Day 07 Part 2 - Not yet finished needs rework of intcode

--- 2019/07/solution.py
+++ 2019/07/solution.py
@@ -16,7 +16,19 @@ def part1(inputStr):
 
 # ANSWER
 def part2(inputStr):
-    return "Part 2 Not Implemented"
+	from itertools import permutations
+	from .. import intcode
+	
+	memory = intcode.parseIntCode(inputStr)
+	finalOutputs = []
+	for phaseSettings in permutations(range(5,10)):
+		previousOutput = 0
+		for pX in phaseSettings:
+			_, previousOutputs = intcode.intCodeRun(memory, [pX, previousOutput])
+			previousOutput = previousOutputs[0]
+			finalOutputs.append(previousOutput)
+			
+	return max(finalOutputs)
 
 # Tests ------------------------------------------
 import unittest
@@ -38,6 +50,14 @@ class tests(unittest.TestCase):
 		self.assertEqual(part1(
 		"3,31,3,32,1002,32,10,32,1001,31,-2,31,1007,31,0,33, 1002,33,7,33,1,33,31,31,1,32,31,31,4,31,99,0,0,0"),
 						 65210)
+	def testExamplePart2Ex1(self):
+		self.assertEqual(part2(
+		"3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26, 27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5"),
+						 139629729)
+	def testExamplePart2Ex2(self):
+		self.assertEqual(part2(
+		"3,52,1001,52,-5,52,3,53,1,52,56,54,1007,54,5,55,1005,55,26,1001,54, -5,54,1105,1,12,1,53,54,53,1008,54,0,55,1001,55,1,55,2,53,55,53,4, 53,1001,56,-1,56,1005,56,6,99,0,0,0,0,10"),
+						 18216)
 
 		
     # def testExamplePart2(self):
