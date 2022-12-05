a1b4f2fbdba0cc18beecb3664740f5e41af01ea1
Benjamin Charlton <kuldrgames@gmail.com>
2022-12-05 21:06:30 +0000

    Completed parsing stacks and therefore completed 2022 day 05 part 1

--- 2022/05/solution.py
+++ 2022/05/solution.py
@@ -3,8 +3,8 @@ def part1(inputStr):
     stackStr, movesStr = inputStr.split("\n\n")
     moves = parseMoves(movesStr) # Number | FromIndex | ToIndex
 
-    # stacks = parseStack(stackStr)
-    stacks = [["Z", "N"], ["M", "C", "D"], ["P"]]
+    stacks = parseStack(stackStr)
+    # stacks = [["Z", "N"], ["M", "C", "D"], ["P"]]
 
     for number, fromIndex, toIndex in moves:
         for _ in range(number):
@@ -16,14 +16,21 @@ def part1(inputStr):
 
     return answer
 
-    raise NotImplementedError("Part 1")
-
 # ANSWER
 def part2(inputStr):
     raise NotImplementedError("Part 2")
 
 def parseStack(stackStr):
-    pass
+    lines = stackStr.split("\n")
+    nStacks = (len(lines[-1]) + 1) // 4
+    
+    stacks = [[] for _ in range(nStacks)]
+    for line in reversed(lines[:-1]):
+        for s in range(nStacks):
+            if (char := line[1+s*4]) != " ":
+                stacks[s].append(char)
+    
+    return stacks
 
 def parseMoves(movesStr):
     moves = []
@@ -46,11 +53,11 @@ class tests(unittest.TestCase):
     # Example tests   
     def testExamplePart1(self):
         self.assertEqual(part1(self.inputStrEx), "CMZ")
-    # def testExamplePart2(self):
-    #     self.assertEqual(part2(self.inputStrEx), 0)
+    def testExamplePart2(self):
+        self.assertEqual(part2(self.inputStrEx), "MCD")
 
-    # # Real Input
-    # def testRealPart1(self):
-    #     self.assertEqual(part1(self.inputStrReal), 0)
+    # Real Input
+    def testRealPart1(self):
+        self.assertEqual(part1(self.inputStrReal), "BWNCQRMDB")
     # def testRealPart2(self):
     #     self.assertEqual(part2(self.inputStrReal), 0)
