9c5caf9ebfe853bda86ac05a9df88e5fdf6311a1
Benjamin Charlton <kuldrgames@gmail.com>
2022-12-08 17:28:02 +0000

    Completed 2022 Day 08 Part 2 - Changed to using imaginary numbers as Coords

--- 2022/08/solution.py
+++ 2022/08/solution.py
@@ -1,10 +1,6 @@
 # 1708
 def part1(inputStr):
-	rows = len(splitInput := inputStr.split("\n"))
-	cols = len(splitInput[0])
-	forest = {(x,y): int(tree) 
-			  for y, row in enumerate(splitInput) 
-			  for x, tree in enumerate(row)}
+	rows, cols, forest = parseTrees(inputStr)
 
 	visible = set()
 
@@ -12,40 +8,66 @@ def part1(inputStr):
 	for x in range(cols):
 		current = -1
 		for y in range(rows):
-			if (next := forest[(x,y)]) > current:
-				visible.add((x,y))
+			if (next := forest[x+y*1j]) > current:
+				visible.add(x+y*1j)
 				current = next
 	# S->N
 	for x in range(cols):
 		current = -1
 		for y in range(rows-1, -1, -1):
-			if (next := forest[(x,y)]) > current:
-				visible.add((x,y))
+			if (next := forest[x+y*1j]) > current:
+				visible.add(x+y*1j)
 				current = next
 	# E->W
 	for y in range(rows):
 		current = -1
 		for x in range(cols):
-			if (next := forest[(x,y)]) > current:
-				visible.add((x,y))
+			if (next := forest[x+y*1j]) > current:
+				visible.add(x+y*1j)
 				current = next
 	# W->E New
 	for y in range(rows):
 		current = -1
 		for x in range(cols-1, -1, -1):
-			if (next := forest[(x,y)]) > current:
-				visible.add((x,y))
+			if (next := forest[x+y*1j]) > current:
+				visible.add(x+y*1j)
 				current = next
 
 	return len(visible)
 
-# ANSWER
+# 504000
 def part2(inputStr):
+	_, _, forest = parseTrees(inputStr)
+
+	scenicScores = {coord: scenicScore(coord, forest)
+					for coord in forest.keys()}
+	
+	return max(scenicScores.values())
+
+def scenicScore(coord, forest):
+	maxHeight = forest[coord]
+	treesInDir = []
+	for direction in [-1j, -1, +1j, +1]:
+		trees = 0
+		while (testCoord := coord+(trees+1)*direction) in forest:
+			if forest[testCoord] < maxHeight:
+				trees += 1
+			else:
+				trees += 1
+				break
+		treesInDir.append(trees)
+
+	from math import prod
+	return prod(treesInDir)
+
+def parseTrees(inputStr):
 	rows = len(splitInput := inputStr.split("\n"))
 	cols = len(splitInput[0])
-	forest = {(x,y): int(tree) 
+	forest = {x+y*1j: int(tree) 
 			  for y, row in enumerate(splitInput) 
 			  for x, tree in enumerate(row)}
+
+	return rows, cols, forest
 
 # Tests ------------------------------------------
 import unittest
@@ -65,5 +87,5 @@ class tests(unittest.TestCase):
     # Real Input
     def testRealPart1(self):
         self.assertEqual(part1(self.inputStrReal), 1708)
-    # def testRealPart2(self):
-    #     self.assertEqual(part2(self.inputStrReal), 0)
+    def testRealPart2(self):
+        self.assertEqual(part2(self.inputStrReal), 504000)
