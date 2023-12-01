72b60faad4c28bcbf081a17fb1369341558304fd
Benjamin Charlton <kuldrgames@gmail.com>
2023-12-01 07:44:21 +0000

    2023 01 Works for part 1 but part 2 needs to consider overlapping values

--- 2023/01/solution.py
+++ 2023/01/solution.py
@@ -1,10 +1,25 @@
-# ANSWER
+# 54708
 def part1(inputStr):
-    raise NotImplementedError("Part 1")
+    import string
+    table = str.maketrans('', '', string.ascii_lowercase)
+    words = inputStr.translate(table).split("\n")
 
-# ANSWER
+    return sum([int(word[0]) * 10 + int(word[-1]) for word in words])
+
+# 53432 too low
 def part2(inputStr):
-    raise NotImplementedError("Part 2")
+    import re
+    nums = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+    for digit, word in enumerate(nums):
+        inputStr = re.sub(word, str(digit+1), inputStr)
+
+    print(inputStr)
+
+    import string
+    table = str.maketrans('', '', string.ascii_lowercase)
+    words = inputStr.translate(table).split("\n")
+
+    return sum([int(word[0]) * 10 + int(word[-1]) for word in words])
 
 # Tests ------------------------------------------
 import unittest
@@ -13,16 +28,19 @@ class tests(unittest.TestCase):
     def setUpClass(cls):
         import importlib.resources
         cls.inputStrEx = importlib.resources.read_text(__package__, "inputEx.txt")
+        cls.inputStrEx2 = importlib.resources.read_text(__package__, "inputEx2.txt")
         cls.inputStrReal = importlib.resources.read_text(__package__, "input.txt")
     
     # Example tests   
     def testExamplePart1(self):
-        self.assertEqual(part1(self.inputStrEx), 0)
+        self.assertEqual(part1(self.inputStrEx), 142)
     def testExamplePart2(self):
-        self.assertEqual(part2(self.inputStrEx), 0)
+        self.assertEqual(part2(self.inputStrEx), 142)
+    def testExample2Part2(self):
+        self.assertEqual(part2(self.inputStrEx2), 281)
 
     # Real Input
     def testRealPart1(self):
-        self.assertEqual(part1(self.inputStrReal), 0)
-    def testRealPart2(self):
-        self.assertEqual(part2(self.inputStrReal), 0)
+        self.assertEqual(part1(self.inputStrReal), 54708)
+    # def testRealPart2(self):
+    #     self.assertEqual(part2(self.inputStrReal), 0)
