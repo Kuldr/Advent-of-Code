ff912c946cfe6f95c544ef680e09ec0d0b2a0b39
Benjamin Charlton <kuldrgames@gmail.com>
2023-12-03 22:51:06 +0000

    Refactor 2023 03 Part 1 ready for part 2

--- 2023/03/solution.py
+++ 2023/03/solution.py
@@ -1,25 +1,10 @@
 # 517021
 def part1(inputStr):
-    import re
-    numberRegex = re.compile(r"(\d+)")
-    symbolRegex = re.compile(r"([^\d|.|\n])")
-    from collections import defaultdict
-    numberCoords = defaultdict(lambda: 0) # 0 so none parts won't effect total
-    symbolCoords = []
-    for y, lineStr in enumerate(inputStr.split("\n")):
-        symbolMatches = symbolRegex.finditer(lineStr)
-        for symbol in symbolMatches:
-            symbolCoords.append(complex(symbol.start(),y))
-            # print(f"{symbol[0]} @ {symbol.start()}+{y}j")
-
-        numberMatches = numberRegex.finditer(lineStr)
-        for number in numberMatches:
-            for x in range(*number.span()): # Tuple unpacking to create range args
-                numberCoords[complex(x, y)] = int(number[0])
-                # print(f"{number[0]} @ {x}+{y}j")
+    numberCoords = parseNumbers(inputStr)
+    symbolCoords = parseSymbols(inputStr)
 
     partTotal = 0
-    for symbolCoord in symbolCoords:
+    for symbolCoord in symbolCoords.keys():
         currentParts = set()
         for offset in [-1-1j,-1,-1+1j,-1j,+1j,+1-1j,+1,+1+1j]:
             currentParts.add(numberCoords[symbolCoord + offset])
@@ -30,6 +15,34 @@ def part1(inputStr):
 # ANSWER
 def part2(inputStr):
     raise NotImplementedError("Part 2")
+
+def parseSymbols(inputStr):
+    import re
+    symbolRegex = re.compile(r"([^\d|.|\n])")
+    symbolCoords = {}
+
+    for y, lineStr in enumerate(inputStr.split("\n")):
+        symbolMatches = symbolRegex.finditer(lineStr)
+        for symbol in symbolMatches:
+            symbolCoords[complex(symbol.start(),y)] = symbol[0]
+            # print(f"{symbol[0]} @ {symbol.start()}+{y}j")
+
+    return symbolCoords
+
+def parseNumbers(inputStr):
+    import re
+    numberRegex = re.compile(r"(\d+)")
+    from collections import defaultdict
+    numberCoords = defaultdict(lambda: 0) # 0 so none parts won't effect total
+
+    for y, lineStr in enumerate(inputStr.split("\n")):
+        numberMatches = numberRegex.finditer(lineStr)
+        for number in numberMatches:
+            for x in range(*number.span()): # Tuple unpacking to create range args
+                numberCoords[complex(x, y)] = int(number[0])
+                # print(f"{number[0]} @ {x}+{y}j")
+    
+    return numberCoords
 
 # Tests ------------------------------------------
 import unittest
