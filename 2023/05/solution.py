3f91d5f6ce9a152cd9dcb3cdb06551ed2a293bd1
Benjamin Charlton <kuldrgames@gmail.com>
2023-12-06 17:00:03 +0000

    Working solution for 2023 05 Part 1 Example but crashes on iPad, suspect memory limitation

---
+++ 2023/05/solution.py
@@ -0,0 +1,61 @@
+# ANSWER
+def part1(inputStr):
+    seeds, maps = parseInput(inputStr)
+    answers = [seedToLocation(seed, maps) for seed in seeds]
+    return min(answers)
+
+# ANSWER
+def part2(inputStr):
+    raise NotImplementedError("Part 2")
+
+def parseInput(inputStr):
+    mapStrs = inputStr.split("\n\n")
+
+    seeds = numbersFromString(mapStrs[0])
+    maps = []
+    for mapStr in mapStrs[1:]: 
+        nextMap = {}
+        for line in mapStr.split("\n")[1:]:
+            destStart, srcStart, rangeLength = numbersFromString(line)
+            for x in range(rangeLength):
+                nextMap[srcStart + x] = destStart + x
+        maps.append(nextMap)
+    
+    return seeds, maps
+
+def seedToLocation(seed, maps):
+    current = seed
+    for mapping in maps:
+        try:
+            current = mapping[current]
+        except KeyError:
+            current = current
+    
+    return current
+    
+def numbersFromString(str):
+    import re
+    numbersRegex = re.compile(r"(\d+)")
+
+    return map(int, numbersRegex.findall(str))
+
+# Tests ------------------------------------------
+import unittest
+class tests(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        import importlib.resources
+        cls.inputStrEx = importlib.resources.read_text(__package__, "inputEx.txt")
+        cls.inputStrReal = importlib.resources.read_text(__package__, "input.txt")
+    
+    # Example tests   
+    def testExamplePart1(self):
+        self.assertEqual(part1(self.inputStrEx), 35)
+    # def testExamplePart2(self):
+    #     self.assertEqual(part2(self.inputStrEx), 0)
+
+    # Real Input
+    def testRealPart1(self):
+        self.assertEqual(part1(self.inputStrReal), 0)
+    # def testRealPart2(self):
+    #     self.assertEqual(part2(self.inputStrReal), 0)
