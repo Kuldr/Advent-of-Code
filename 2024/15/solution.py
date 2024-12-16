6e631beae9e504b5033a77712e2c375da2526350
Ben Charlton <kuldrgames@gmail.com>
2024-12-16 22:27:09 +0000

    2024 15 Part 2 Solved needs major refactor

--- 2024/15/solution.py
+++ 2024/15/solution.py
@@ -6,9 +6,11 @@ def part1(inputStr):
 
     dirs = {"^": -1j, "v": +1j, "<": -1, ">": +1}
     for move in moves:
-        canSwap, toSwap = gridMove(grid, robotCoord, dirs[move], {robotCoord: "."})
+        canSwap, toSwap = gridMove(grid, robotCoord, dirs[move], {})
         if canSwap:
             robotCoord += dirs[move]
+            for coord in toSwap.keys():
+                grid[coord-dirs[move]] = "."
             for coord, char in toSwap.items():
                 grid[coord] = char
 
@@ -18,18 +20,23 @@ def part1(inputStr):
     
     return sum(gpsCoords)
     
-# ANSWER
+# 1472235
 def part2(inputStr):            
     grid, moves, maxX, maxY = parseInput2(inputStr)
 
     robotCoord = [coord for coord, char in grid.items() if char == "@"][0]
 
     dirs = {"^": -1j, "v": +1j, "<": -1, ">": +1}
-    for move in moves:
-        if gridMove2(grid, robotCoord, dirs[move]):
+    for i, move in enumerate(moves):
+        canSwap, toSwap = gridMove2(grid, robotCoord, dirs[move], {})
+        if canSwap:
             robotCoord += dirs[move]
+            for coord in toSwap.keys():
+                grid[coord-dirs[move]] = "."
+            for coord, char in toSwap.items():
+                grid[coord] = char
 
-    printGrid(grid, maxX, maxY)
+    # printGrid(grid, maxX, maxY)
 
     gpsCoords = [100 * coord.imag + coord.real for coord, char in grid.items() if char == "["]
     
@@ -92,49 +99,43 @@ def gridMove(grid, coord, dir, toSwap):
         case "#":
             # Can't swap
             return False, toSwap
-        
-def gridMove2(grid, coord, dir):
-    if dir == -1 or dir == +1: # horizontal movement mostly unchanged
+
+def gridMove2(grid, coord, dir, toSwap):
+    # Might be able to clear up the if with recursion loop checking
+    if dir == -1 or dir == +1: # Horizontal Movement
         match grid[nextCoord := coord + dir]:
             case ".":
                 # Swap
-                grid[coord], grid[nextCoord] = grid[nextCoord], grid[coord]
-                return True
+                toSwap[nextCoord] = grid[coord]
+                return True, toSwap
             case "[" | "]":
                 # Check Next
-                if gridMove2(grid, nextCoord, dir):
-                    grid[coord], grid[nextCoord] = grid[nextCoord], grid[coord]
-                    return True
-                else:
-                    return False
+                toSwap[nextCoord] = grid[coord]
+                return gridMove2(grid, nextCoord, dir, toSwap)
             case "#":
                 # Can't swap
-                return False
-    else: # Vertical movement
+                return False, toSwap
+    else: # Vertical Movement
         match grid[nextCoord := coord + dir]:
             case ".":
                 # Swap
-                grid[coord], grid[nextCoord] = grid[nextCoord], grid[coord]
-                return True
+                toSwap[nextCoord] = grid[coord]
+                return True, toSwap
             case "[":
                 # Check Next
-                if gridMove2(grid, nextCoord, dir) and gridMove2(grid, nextCoord+1, dir) and gridMove2(grid, coord+1, dir):
-                    grid[coord], grid[nextCoord] = grid[nextCoord], grid[coord]
-                    grid[coord+1], grid[nextCoord+1] = grid[nextCoord+1], grid[coord+1]
-                    return True
-                else:
-                    return False
+                toSwap[nextCoord] = grid[coord]
+                leftCheck, toSwap = gridMove2(grid, nextCoord, dir, toSwap)
+                rightCheck, toSwap = gridMove2(grid, nextCoord+1, dir, toSwap)
+                return leftCheck and rightCheck, toSwap
             case "]":
                 # Check Next
-                if gridMove2(grid, nextCoord, dir) and gridMove2(grid, nextCoord-1, dir) and gridMove2(grid, coord-1, dir):
-                    grid[coord], grid[nextCoord] = grid[nextCoord], grid[coord]
-                    grid[coord-1], grid[nextCoord-1] = grid[nextCoord-1], grid[coord-1]
-                    return True
-                else:
-                    return False
+                toSwap[nextCoord] = grid[coord]
+                rightCheck, toSwap = gridMove2(grid, nextCoord, dir, toSwap)
+                leftCheck, toSwap = gridMove2(grid, nextCoord-1, dir, toSwap)
+                return leftCheck and rightCheck, toSwap
             case "#":
                 # Can't swap
-                return False
+                return False, toSwap
 
 # Tests ------------------------------------------
 import unittest
@@ -144,6 +145,8 @@ class tests(unittest.TestCase):
         import importlib.resources
         cls.inputStrEx = importlib.resources.read_text(__package__, "inputEx.txt")
         cls.inputStrEx2 = importlib.resources.read_text(__package__, "inputEx2.txt")
+        cls.inputStrCus = importlib.resources.read_text(__package__, "inputCustom.txt")
+        cls.inputStrCus2 = importlib.resources.read_text(__package__, "inputCustom2.txt")
         cls.inputStrReal = importlib.resources.read_text(__package__, "input.txt")
     
     # Example tests   
@@ -151,14 +154,18 @@ class tests(unittest.TestCase):
         self.assertEqual(part1(self.inputStrEx), 10092)
     def testExample2Part1(self):
         self.assertEqual(part1(self.inputStrEx2), 2028)
-    # def testExamplePart2(self):
-    #     self.assertEqual(part2(self.inputStrEx), 9021)
+    def testExamplePart2(self):
+        self.assertEqual(part2(self.inputStrEx), 9021)
+    def testCustomPart2(self):
+        self.assertEqual(part2(self.inputStrCus), 102)
+    def testCustom2Part2(self):
+        self.assertEqual(part2(self.inputStrCus2), 304)
 
     # Real Input
     def testRealPart1(self):
         self.assertEqual(part1(self.inputStrReal), 1448589)
-    # def testRealPart2(self):
-    #     self.assertEqual(part2(self.inputStrReal), 0)
+    def testRealPart2(self):
+        self.assertEqual(part2(self.inputStrReal), 1472235)
 
 # Run Main ------------------------------------------
 if __name__ == "__main__":
