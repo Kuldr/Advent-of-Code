a5694fa944f4d5f5bd8015b37c4916ddd5382ac2
Ben Charlton <kuldrgames@gmail.com>
2026-01-03 13:22:59 +0000

    2025 03 Part 2 Regex attempt intractable

--- 2025/03/solution.py
+++ 2025/03/solution.py
@@ -1,26 +1,14 @@
 # 17193
 def part1(inputStr):
     banks = parseInput(inputStr)
-    return largestBatteryCombinations(banks, 2)
+    # return largestBatteryCombinations(banks, 2)
+    return largestBatteryRegex(inputStr, 2)
 
 # ANSWER
 def part2(inputStr):
     banks = parseInput(inputStr)
-    return largestBatteryCombinations(banks, 12)
-
-    # sumBatteries = 0
-
-    # import re
-    
-    # from time import time
-    # startTime = time()
-
-    # for i in range(111_111_111_111, 999_999_999_999):
-    #     # regex => (.*[9].*[9].*[9].*[9].*[9].*[9].*[9].*[9].*[9].*[9].*[9].*[9].*)
-    #     regex = "(.*[" + "].*[".join(str(i)) + "].*)"
-    #     if found := re.findall(regex, inputStr):
-    #         sumBatteries += i
-    #         inputStr = inputStr.replace(found[0], "")
+    # return largestBatteryCombinations(banks, 12)
+    return largestBatteryRegex(inputStr, 12)
 
 def parseInput(inputStr):
     return [line for line in inputStr.split("\n")]
@@ -37,6 +25,42 @@ def largestBatteryCombinations(banks, cells):
 
     return sumBatteries
 
+# Intractable for part 2 takes years of run time
+def largestBatteryRegex(inputStr, cells):
+    sumBatteries = 0
+
+    import re
+    
+    # from time import time
+    # startTime = time()
+
+    from itertools import product
+    combos = product("987654321", repeat=cells)
+    count = 0
+
+    itemsToSearch = 9**cells
+
+    for i, combo in enumerate(combos, start=1):
+        combo = int("".join(combo))
+        # regex => (.*[9].*[9].*) for 2 cells
+        regex = "(.*[" + "].*[".join(str(combo)) + "].*)"
+        if found := re.findall(regex, inputStr):
+            for x in found:
+                sumBatteries += combo
+                inputStr = inputStr.replace(x, "")
+        # All Banks solved finish search
+        if inputStr.isspace():
+            break
+
+        # if i % 100_000 == 0:
+        #     timeTakenPerItem = (time()-startTime)/(i)
+        #     itemsRemaining = itemsToSearch - i
+        #     print(f"timeRemaining: {timeTakenPerItem*itemsRemaining}s")
+        #     print(i)
+        #     print(f"{i/itemsToSearch*100}%")
+
+    return sumBatteries
+
 # Tests ------------------------------------------
 import unittest
 class tests(unittest.TestCase):
@@ -49,14 +73,14 @@ class tests(unittest.TestCase):
     # Example tests   
     def testExamplePart1(self):
         self.assertEqual(part1(self.inputStrEx), 357)
-    def testExamplePart2(self):
-        self.assertEqual(part2(self.inputStrEx), 3121910778619)
+    # def testExamplePart2(self):
+    #     self.assertEqual(part2(self.inputStrEx), 3121910778619)
 
     # Real Input
     def testRealPart1(self):
         self.assertEqual(part1(self.inputStrReal), 17193)
-    def testRealPart2(self):
-        self.assertEqual(part2(self.inputStrReal), 0)
+    # def testRealPart2(self):
+    #     self.assertEqual(part2(self.inputStrReal), 0)
 
 # Run Main ------------------------------------------
 if __name__ == "__main__":
